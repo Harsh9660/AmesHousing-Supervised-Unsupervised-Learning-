0c9ca4b22969d443855d80f1d1b528a71b5b9bee
harsh pandya <harsh.pandya@vrinsoft.com>
2025-11-19 11:11:11 +0530

    Updated

--- Unsupervised_Learning.py
+++ Unsupervised_Learning.py
@@ -7,3 +7,19 @@ fig = plt.figure()
 ax = fig.add_subplot(111)
 plt.grid(True)
 plt.scatter(X[:,0], X[:,1], c=y)
+plt.show()
+
+k = 3
+clusters = {i: [] for i in range(k)}
+np.random.seed(23)
+
+for idx in range(len(k)):
+    center = 2*(2*np.random.random((X.shape[1],)) - 1)
+    points = []
+    clusters = {
+        'clusters': center,
+        'points': points
+    }
+
+    clusters[idx] = clusters
+clusters
